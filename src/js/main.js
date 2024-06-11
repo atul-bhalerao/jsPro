b60de27c824fcdf56a1854187778b09ac958c2a7
Atul Bhalerao <atulyaaa.bhalerao@gmail.com>
2024-06-11 21:18:05 +0530

    DOM manupalution

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+// Import our custom CSS
+import '../sass/styles.sass'
+
+// Import all of Bootstrap's JS
+// import * as bootstrap from 'bootstrap'
+
+document.getElementById("demo").innerHTML = "Hello JavaScript";
+
+var $demo = document.getElementsByClassName("demoOne");
+
+$demo[0].innerHTML = "Hello JavaScript";
